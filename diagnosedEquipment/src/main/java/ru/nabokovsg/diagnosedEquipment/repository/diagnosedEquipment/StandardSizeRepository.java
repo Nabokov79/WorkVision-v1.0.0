36585226657b745594dd7c2f0fbedeb2bc7e0002
Nabokov79 <nabokovsg@yandex.ru>
2024-08-11 19:11:57 +0300

    delete

--- diagnosedEquipment/src/main/java/ru/nabokovsg/diagnosedEquipment/repository/diagnosedEquipment/StandardSizeRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.nabokovsg.diagnosedEquipment.repository.diagnosedEquipment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nabokovsg.diagnosedEquipment.model.diagnosedEquipment.StandardSize;
-
-public interface StandardSizeRepository extends JpaRepository<StandardSize, Long> {
-}
