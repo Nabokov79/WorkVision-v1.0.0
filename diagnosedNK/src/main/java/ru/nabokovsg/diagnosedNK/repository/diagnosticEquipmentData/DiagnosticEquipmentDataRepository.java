d1322375667fdfd49ae08741ec601fe0a87c6391
Nabokov79 <nabokovsg@yandex.ru>
2024-08-11 19:11:45 +0300

    delete

--- diagnosedNK/src/main/java/ru/nabokovsg/diagnosedNK/repository/diagnosticEquipmentData/DiagnosticEquipmentDataRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.nabokovsg.diagnosedNK.repository.diagnosticEquipmentData;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nabokovsg.diagnosedNK.model.diagnosticEquipmentData.DiagnosticEquipmentData;
-
-public interface DiagnosticEquipmentDataRepository extends JpaRepository<DiagnosticEquipmentData, Long> {
-
-    DiagnosticEquipmentData findByEquipmentId(Long equipmentId);
-}
