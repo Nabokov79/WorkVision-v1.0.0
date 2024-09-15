908d86247855296f2495bcadd5cfa4fdadece558
Nabokov79 <nabokovsg@yandex.ru>
2024-09-15 19:12:39 +0300

    delete

--- diagnosedNK/src/main/java/ru/nabokovsg/diagnosedNK/service/constantService/ConstParameterMeasurementService.java
+++
@@ -1,8 +0,0 @@
-package ru.nabokovsg.diagnosedNK.service.constantService;
-
-public interface ConstParameterMeasurementService {
-
-    String getParameterName(String measuredParameter);
-
-    String getUnitMeasurement(String unitMeasurement);
-}
